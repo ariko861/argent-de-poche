8544fc7f436c1035ab781390131328a12b66c540
Arthur Briquet <mdpbriar@henallux.be>
2023-12-12 14:43:14 +0100

    Corrections model Compte

--- app/Models/Compte.ts
+++ app/Models/Compte.ts
@@ -1,19 +1,19 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
-import User from "App/Models/User";
+import User from 'App/Models/User'
 
 export default class Compte extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
   @column()
-  public name: string;
+  public name: string
 
   @column()
-  public total: number;
+  public total: number
 
   @column()
-  public salary: number;
+  public salary: number
 
   @belongsTo(() => User)
   public owner: BelongsTo<typeof User>
